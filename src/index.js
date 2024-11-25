92bb5f4eb1b966335ef8c7d7457bb7b13647f058
Aanvil7 <sven.groot@student.hu.nl>
2024-11-25 23:36:49 +0100

    update button color and make site scale for mobile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';  // Make sure no '.js' extension is needed here
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
