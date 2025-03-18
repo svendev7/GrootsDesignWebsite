b47623a9df0cf9dd3354db2950de0455b38527ea
Aanvil7 <113886810+Aanvil7@users.noreply.github.com>
2025-03-18 21:22:50 +0100

    update header, new images, and projects mobile and smaller screen rework(its still shit imo but thats the solutrion the owner wanted)

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -4,17 +4,66 @@ import './projects.css';
 import InspirationSection from '../../components/InspirationSection/InspirationSection';
 
 const Projects = () => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const isStackedLayout = windowWidth < 1920;
   return (
     <div className="projects">
       <Header />
       <div className="projects-content">
-
+      {isStackedLayout ? (
+          <>
+            <div className="stacked-image">
+              <img src="/images/1.jpg" alt="Project 1" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/10.jpg" alt="Project 2" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/2.jpg" alt="Project 3" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/3.jpg" alt="Project 4" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/2.jpg" alt="Project 5" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/6.jpg" alt="Project 6" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/5.jpg" alt="Project 7" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/7.jpg" alt="Project 8" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/8.jpg" alt="Project 9" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/4.jpg" alt="Project 10" />
+            </div>
+            <div className="stacked-image">
+              <img src="/images/9.jpg" alt="Project 11" />
+            </div>
+          </>
+        ) : (
+          <>
         <div className="top-row">
           <div className="large-image">
             <img src="/images/1.jpg" alt="Project 1" />
           </div>
           <div className="small-image">
-            <img src="/images/9.png" alt="Project 2" />
+            <img src="/images/10.jpg" alt="Project 2" />
           </div>
         </div>
 
@@ -24,7 +73,7 @@ const Projects = () => {
           </div>
           <div className="right-column">
             <div className="top-right-image">
-              <img src="/images/3.png" alt="Project 4" />
+              <img src="/images/3.jpg" alt="Project 4" />
             </div>
             <div className="bottom-right-images">
               <img src="/images/2.jpg" alt="Project 5" />
@@ -38,14 +87,16 @@ const Projects = () => {
         </div>
 
         <div className="three-column">
-          <img src="/images/7.png" alt="Project 8" />
-          <img src="/images/8.png" alt="Project 9" />
+          <img src="/images/7.jpg" alt="Project 8" />
+          <img src="/images/8.jpg" alt="Project 9" />
           <img src="/images/4.jpg" alt="Project 10" />
         </div>
 
         <div className="full-width-image">
-          <img src="/images/6.png" alt="Project 11" />
-        </div>
+          <img src="/images/9.jpg" alt="Project 11" />
+          </div>
+          </>
+        )}
       </div>
       <InspirationSection />
     </div>
